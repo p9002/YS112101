16b13a380cf49d2abcf3379073733728cdce1376
alan <p9002>
2016-11-21 10:09:18 +0800

    MyAdapter加入按鍵

--- app/src/main/java/alan/no1/ys112101/MyAdapter.java
+++ app/src/main/java/alan/no1/ys112101/MyAdapter.java
@@ -4,13 +4,16 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by user on 2016/11/21.
  */
 
-public class MyAdapter {
+public class MyAdapter extends BaseAdapter {
     Context context;
     String[] data;
     public MyAdapter(Context context, String[] d)
@@ -33,11 +36,19 @@ public class MyAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, final View convertView, ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View v = inflater.inflate(R.layout.myitem, null);
         TextView tv = (TextView) v.findViewById(R.id.textView);
         tv.setText(data[position]);
+
+        Button btn = (Button) v.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, data[position], Toast.LENGTH_SHORT).show();
+            }
+        });
         return v;
     }
 }
